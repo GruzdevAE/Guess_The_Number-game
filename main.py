1c1e4d3fc7a5cd2f7ec5b1a56ce0ce4744f8aaf4
Alexander Gruzdev <gruzdevae@yahoo.com>
2016-06-25 16:18:03 +0300

    Update main.py
    Проверены все доступные режимы, исправлены ошибки, небольшие изменения в выводе сообщений.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from os import system
 from random import randrange, shuffle, choice
 
-def highscore(score, name):
-    pass
+# def highscore(score, name):
+#     pass
 
 init = ''
 max_attempts = 0
@@ -78,17 +78,20 @@ while game_choice != 'Выход':
                                   '\nЗнаю я тебя, сейчас, в процессе игры, ещё десять раз поменяешь число.' +
                                   '\nДавай пиши сюда. И сам не забудешь, и я буду уверен что ты не мухлюешь.\n')
                     if cheat:
-                        print('Таааак. Мне кажется, это число', cheat, '. Ну что, я угадал? Можешь не отвечать, я и так знаю что угадал, я же великолепен!')
+                        print('Таааак. Мне кажется, это число', cheat, '. Ну что, я угадал? Можешь не отвечать,' +
+                              ' я и так знаю что угадал, я же великолепен!')
                     else:
                         print('А тебя не так просто провести. Ну что ж, так даже интересней.')
-                        print('Давай условимся отвечать строго является ли моё число \'Больше\', \'Меньше\' или же я \'Угадал\'')
+                        print('Давай условимся отвечать строго является ли моё число \'Больше\', \'Меньше\' ' +
+                              'или же я \'Угадал\'')
                         answer = ''
                         min_num_pc = 0
                         while answer != 'Угадал':
                             guess_pc = int(max_num_pc) - (int(max_num_pc)-min_num_pc)//2
                             answer = input(guess_pc).capitalize()
                             if guess_pc == max_num_pc:
-                                print('Эй! Да ты жульничаешь! Я так и знал! Ну да ладно, я и сам, ведь, пытался. Так что мы квиты, да?')
+                                print('Эй! Да ты жульничаешь! Я так и знал! Ну да ладно, я и сам, ведь, пытался.' +
+                                      ' Так что мы квиты, да?')
                                 input('Жми \'Enter\' для возврата в меню.')
                                 break
                             if answer == 'Больше':
@@ -121,18 +124,18 @@ while game_choice != 'Выход':
                         shuffle(jumble)
                         [print(letter, end='') for letter in jumble]
                         points = len(jumble)*10
-                        print('\nНу как тебе? Можешь начинать угадывать. ' + 
+                        print('\nНу как тебе? Можешь начинать угадывать. ' +
                               'За это слово ты сможешь получить ' + str(points) + ' очков.')
                         answer = None
                         while answer != word:
-                            answer = input('Пиши слово: ').lower()
+                            answer = input(answer_message).lower()
                             if not answer:
                                 empty = input('Хочешь подсказку или сдаешься?').capitalize()
                                 if empty == 'Подсказка':
-                                    n += 1
-                                    points = (len(jumble)-n)*10
+                                    hlp += 1
+                                    points = (len(jumble)-hlp)*10
                                     print('Вот тебе подсказка:')
-                                    answer_message = 'Подсказка: ' + str(word[:n]) + '\nПиши слово: '
+                                    answer_message = 'Подсказка: ' + word[:hlp] + '\nПиши слово: '
                                 elif empty == 'Сдаюсь':
                                     print('Ха-ха, я выиграл!')
                                     input('Дави \'Enter\', чтобы продолжить.')
